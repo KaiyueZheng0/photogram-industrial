9f8447a31250396ed8caf50db0fc7d545a593cf3
KaiyueZheng0 <kzheng0@chicagobooth.edu>
2025-04-09 16:27:24 +0000

    Set up users#show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,11 @@
 Rails.application.routes.draw do
+  root "photos#index
+
   devise_for :users
 
-  resources :photos
   resources :comments
-  resources :likes
   resources :follow_requests
-
-  # Custom user routes
-  get "/:username", to: "users#show", as: :user_profile
-  get "/:username/feed", to: "users#feed", as: :user_feed
-  get "/:username/discover", to: "users#discover", as: :user_discover
-
-  # Devise root handling
-  devise_scope :user do
-    authenticated :user do
-      root to: redirect { |_, req|
-        user = req.env['warden'].user
-        user.present? ? "/#{user.username}/feed" : "/users/sign_in"
-      }, as: :authenticated_root
-    end
-
-    unauthenticated do
-      root to: "devise/sessions#new", as: :unauthenticated_root
-    end
-  end
+  resources :likes
+  resources :photos
+  resources: users, only: :show
 end
